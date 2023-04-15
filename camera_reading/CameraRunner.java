e0aa4dd6c27539fb609c254b5dd2c5f21a7d6a8f
AdarshRIyer <76015889+adarshramiyer@users.noreply.github.com>
2023-04-15 10:37:29 -0700

    configured opencv

--- camera_reading/CameraRunner.java
+++ camera_reading/CameraRunner.java
@@ -1,5 +1,13 @@
-package camera_read;
+import java.lang.Math;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
 
 public class CameraRunner {
+    public static void main(String[] args) {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
+        System.out.println(mat.dump());
+    }
     
 }
