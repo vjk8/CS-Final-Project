7302d24596acc2ff898d8e42f07310cf6c27812c
AdarshRIyer <76015889+adarshramiyer@users.noreply.github.com>
2023-05-05 09:59:10 -0700

    audio capture now working

--- CameraStarter.java
+++ CameraStarter.java
@@ -1,8 +1,6 @@
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.DataLine;
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.TargetDataLine;
+import javax.sound.sampled.*;
+import javax.xml.crypto.Data;
+import java.util.*;
 
 public class CameraStarter {
     private int threshold;
@@ -19,6 +17,7 @@ public class CameraStarter {
 
     public static TimeFormat getStartTime()
     {
+        ArrayList<Byte> allData = new ArrayList<Byte>();
         TargetDataLine line = getTargetDataLine();
         if (line == null) return null;
         int numBytesRead;
@@ -28,15 +27,24 @@ public class CameraStarter {
         while (System.currentTimeMillis() - startTime < 1000) {
             numBytesRead = line.read(data, 0, data.length);
             System.out.println(numBytesRead + " bytes read at time " + (System.currentTimeMillis() - startTime));
+            System.out.print("Sample data: [");
+            for (int i = 0; i < 10; i++) {
+                System.out.print(data[i] + ",");
+            }
+            System.out.println("]");
+            for (byte b : data) {
+                allData.add(b);
+            }
         }
-        // TODO complete method
         return null;
     }
 
     private static TargetDataLine getTargetDataLine() {
-        AudioFormat format = new AudioFormat(44100.0f, 64, 1, false, true);
+        AudioFormat format = new AudioFormat(44100f, 8, 1, true, true);
         TargetDataLine line;
         DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+
+        System.out.println(info.getFormats());
 
         if (!AudioSystem.isLineSupported(info)) {
             System.out.println("AUDIO LINE NOT SUPPORTED");
@@ -53,10 +61,17 @@ public class CameraStarter {
         return line;
     }
 
-    public static void main(String[] args) { // for testing purposed only
+    public static void main(String[] args) { // for testing purposes only
         TimeFormat t = getStartTime();
+        DataPlotter dp = new DataPlotter();
+        dp.run();
     }
 }
 
-class PlotSoundData { // for testing purposed later
+class DataPlotter { // for testing purposes later
+    public DataPlotter() {};
+
+    public static void run() {
+        // TODO figure out how to plot data
+    }
 }
