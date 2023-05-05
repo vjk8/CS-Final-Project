ca183d77f7a8557a075f0d6575fe5912199e3db6
AdarshRIyer <76015889+adarshramiyer@users.noreply.github.com>
2023-05-05 09:05:50 -0700

    TargetDataLine getting for CameraStarter

--- CameraStarter.java
+++ CameraStarter.java
@@ -1,5 +1,7 @@
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
 
 public class CameraStarter {
@@ -17,11 +19,30 @@ public class CameraStarter {
 
     public TimeFormat getStartTime()
     {
+        TargetDataLine line = getTargetDataLine();
+        if (line == null) return null;
+        // TODO complete method
+        return null;
+    }
+
+    private TargetDataLine getTargetDataLine() {
         AudioFormat format = new AudioFormat(44100.0f, 64, 1, false, true);
         TargetDataLine line;
         DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
-        // TODO complete method
-        return null;
+
+        if (!AudioSystem.isLineSupported(info)) {
+            System.out.println("AUDIO LINE NOT SUPPORTED");
+            return null;
+        }
+        try {
+            line = (TargetDataLine) AudioSystem.getLine(info);
+            line.open(format);
+        }
+        catch (LineUnavailableException ex) {
+            System.out.println("AUDIO LINE UNAVAILABLE");
+            return null;
+        }
+        return line;
     }
 }
 
