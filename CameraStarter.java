54a2eece017c0f1fbc0f487642a912042861e8c5
AdarshRIyer <76015889+adarshramiyer@users.noreply.github.com>
2023-05-05 09:13:32 -0700

    audio line not supported...

--- CameraStarter.java
+++ CameraStarter.java
@@ -17,15 +17,23 @@ public class CameraStarter {
         threshold = th;
     }
 
-    public TimeFormat getStartTime()
+    public static TimeFormat getStartTime()
     {
         TargetDataLine line = getTargetDataLine();
         if (line == null) return null;
+        int numBytesRead;
+        byte[] data = new byte[line.getBufferSize() / 5];
+        line.start();
+        long startTime = System.currentTimeMillis();
+        while (System.currentTimeMillis() - startTime < 1000) {
+            numBytesRead = line.read(data, 0, data.length);
+            System.out.println(numBytesRead + " bytes read at time " + (System.currentTimeMillis() - startTime));
+        }
         // TODO complete method
         return null;
     }
 
-    private TargetDataLine getTargetDataLine() {
+    private static TargetDataLine getTargetDataLine() {
         AudioFormat format = new AudioFormat(44100.0f, 64, 1, false, true);
         TargetDataLine line;
         DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
@@ -43,6 +51,10 @@ public class CameraStarter {
             return null;
         }
         return line;
+    }
+
+    public static void main(String[] args) { // for testing purposed only
+        TimeFormat t = getStartTime();
     }
 }
 
