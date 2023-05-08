e80dd7a990772651321ef603bc45fcd7970a97d5
adarshramiyer <adarshramiyer@users.noreply.github.com>
2023-05-08 19:35:33 +0000

    Apply formatting changes

--- tests/CameraStarterTester.java
+++ tests/CameraStarterTester.java
@@ -1,12 +1,13 @@
 package tests;
 // some git error
+import components.CameraStarter;
 import org.junit.Assert.*;
 import org.junit.Test;
-import components.CameraStarter;
 
 public class CameraStarterTester {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         CameraStarter starter = new CameraStarter(128); // sound-based
         System.out.println("Block for 10 seconds while waiting for sound");
         long result = starter.getStartTime();
