214f43f352665faf4a599f90c393e2bde50009e3
adarshramiyer <adarshramiyer@users.noreply.github.com>
2023-05-10 16:39:50 +0000

    Apply formatting changes

--- tests/CameraRunnerTester.java
+++ tests/CameraRunnerTester.java
@@ -2,7 +2,8 @@ package tests;
 import components.*;
 
 public class CameraRunnerTester {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         CameraRunner cr = new CameraRunner();
         cr.run();
     }
