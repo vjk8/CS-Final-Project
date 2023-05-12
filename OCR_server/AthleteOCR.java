48a1ee4a71233cff13d7ffcd8fe2f1e43d633881
jeffarjeffar <jeffarjeffar@users.noreply.github.com>
2023-05-12 03:26:23 +0000

    Apply formatting changes

--- OCR_server/AthleteOCR.java
+++ OCR_server/AthleteOCR.java
@@ -10,7 +10,7 @@ public class AthleteOCR {
 
     public static native int getAthleteNumber(Mat img);
 
-	public static void main(String[] args) {
-		System.out.println("Test");
-	}
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
 }
