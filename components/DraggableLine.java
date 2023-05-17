7e6214fcc31d9f7ae2f337ed498e6c6428fbfd05
vjk8 <vijay@krishnamoorthy.com>
2023-05-17 09:53:41 -0700

    commit

--- components/DraggableLine.java
+++ components/DraggableLine.java
@@ -1,15 +1,12 @@
 package components;
 
 import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.event.MouseListener;
 
-public class DraggableLine implements Comparable, MouseListener {
+public class DraggableLine implements Comparable {
     private Color color;
     private TimeFormat timestamp;
     private String hipNumberLabel;
     private int xPos;
-    private boolean dragged = false;
 
     public DraggableLine() {
         // TODO complete no-args constructor
@@ -25,6 +22,17 @@ public class DraggableLine implements Comparable, MouseListener {
         xPos = x;
     }
 
+    public int getXPos()
+    {
+        return xPos;
+    }
+
+    public int changeXPos(int newX)
+    {
+        xPos = newX;
+        return xPos;
+    }
+
     public void setColor() {
         // TODO complete method
         color = Color.red;
@@ -36,90 +44,11 @@ public class DraggableLine implements Comparable, MouseListener {
         timestamp = c.getTimeAtPixel(xPos);
     }
 
-    public boolean isDragged()
-    {
-        return dragged;
-    }
-
     public int compareTo(Object other)
     {
         // TODO complete comparator
 
         return timestamp.compareTo(other);
     }
-
-
-    public void paintComponent(Graphics g) 
-    {
-        g.setColor(color);
-        g.drawLine(xPos, 0, xPos, 200);
-    }
-
-    @Override
-    public void mouseClicked(java.awt.event.MouseEvent e) {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
-    // get new x position
-    public void mousePressed(java.awt.event.MouseEvent e) {
-        // TODO Auto-generated method stub
-        xPos = e.getX();
-    }
-
-    @Override
-    // translate the point by adding the x position, y value does not change
-    public void mouseReleased(java.awt.event.MouseEvent e) {
-        // TODO Auto-generated method stub
-        e.translatePoint(xPos, 0);
-    }
-
-    @Override
-    public void mouseEntered(java.awt.event.MouseEvent e) {
-        // TODO Auto-generated method stub
-        dragged = true;
-        
-    }
-
-    @Override
-    public void mouseExited(java.awt.event.MouseEvent e) {
-        // TODO Auto-generated method stub
-    }
-
-        /*
-
-    Point curr;
-    Point prev;
-     * public TApp()
-    {
-        curr = new Point(0,0);
-        ClickListener clickListener = new ClickListener();
-        this.addMouseListener(clickListener);
-        DragListener dragListener = new DragListener();
-        this.addMouseMotionListener(dragListener);
-    }
-
-    public void paintComponent(Graphics g)
-    {
-        super.paintComponent(g);
-        g.drawLine((int)curr.getX(), 0, (int)curr.getX(), 200);
-    }
-    private class ClickListener extends MouseAdapter{
-        public void mousePressed(MouseEvent e){
-            prev = new Point(e.getScreenX(), e.getScreenY());
-        }
-    }
-
-    private class DragListener extends MouseMotionAdapter{
-        public void mouseDragged(MouseEvent e)
-        {
-            Point current = new Point(e.getScreenX(), e.getScreenY());
-
-            curr.translate((int)(current.getX() - prev.getX()), (int)(current.getY() - prev.getY()));
-
-            prev = current;
-            repaint();
-        }
-    }
-     */
+    
 }
--- components/PostTimingGUI.java
+++ components/PostTimingGUI.java
@@ -1,41 +1,110 @@
 
 package components;
-import java.awt.event.MouseAdapter;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.util.*;
-import javax.swing.JPanel;
+import javax.swing.JFrame;
+import java.awt.event.MouseListener;
 import org.opencv.core.*;
 
-public class PostTimingGUI extends JPanel {
+public class PostTimingGUI extends JFrame {
     private ArrayList<DraggableLine> finishes;
-    private CompositeFrame finishImage;
+    private static CompositeFrame finishImage;
     private OutputProcessor processor;
+    private int xPos = 10;
 
     public PostTimingGUI(CompositeFrame image) {
         // TODO complete constructor
+        finishes = new ArrayList<DraggableLine>();
+        finishes.add(new DraggableLine(new TimeFormat(), "a", 100));
+        addMouseListener (new MouseListener() {
+
+
+            @Override
+            public void mouseClicked(java.awt.event.MouseEvent e)
+            {
+                xPos = e.getX();
+            }
+ 
+ 
+            @Override
+            public void mousePressed(java.awt.event.MouseEvent e)
+            {
+            }
+ 
+ 
+            @Override
+            public void mouseReleased(java.awt.event.MouseEvent e)
+            {
+                
+                for (DraggableLine d: finishes)
+                {
+                    if (d.getXPos() == xPos)
+                    {
+                        finishes.get(finishes.indexOf(d)).changeXPos(e.getX());
+                    }
+                }
+                xPos = e.getX();
+                e.translatePoint(xPos, 0);
+                repaint();
+                
+            }
+ 
+ 
+            @Override
+            public void mouseEntered(java.awt.event.MouseEvent e)
+            {
+              
+            }
+ 
+ 
+            @Override
+            public void mouseExited(java.awt.event.MouseEvent e)
+            {
+               
+            }
+        });
+        setTitle("hi");
+        setSize(200, 200);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
         finishImage = image;
         finishes = new ArrayList<DraggableLine>();
         processor = new OutputProcessor(finishes);
     }
 
+    public void paint(Graphics g)
+    {
+        g.setColor(Color.RED);
+        for (DraggableLine d: finishes)
+        {
+            g.drawLine(d.getXPos(), 0, d.getXPos(), this.getHeight());
+        }
+
+    }
+
+    public void removeLine(int x, int y)
+    {
+
+    }
+
+
+
     private ArrayList<DraggableLine> getOCR() {
         // TODO don't worry about this one right now
         // each draggable line gets the ocr, every time the draggable line is moved
-        for (DraggableLine d: finishes)
-        {
-            if (d.isDragged());
-        }
         return null;
     }
 
-    public void run() {
+    public static void run() {
         // TODO GUI code, treat like a main method
-        JFrame frame = new JFrame();
-        frame.setSize(200, 200);
-        for (DraggableLine d: finishes)
-        {
-            frame.addMouseListener(d);
-        }
-        frame.setVisible(true);
+        PostTimingGUI run = new PostTimingGUI(finishImage);
     }
+
+    public static void main(String[] args)
+    {
+        run();
+    }
+    
 }
