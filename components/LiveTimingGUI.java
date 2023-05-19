1d5e393f5b69bdd0c1436f6354dc9552c6b21876
AdarshRIyer <76015889+adarshramiyer@users.noreply.github.com>
2023-05-19 10:00:27 -0700

    working out bugs with LiveTimingGUI

--- components/LiveTimingGUI.java
+++ components/LiveTimingGUI.java
@@ -8,23 +8,29 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import org.opencv.core.Core;
+import org.opencv.core.CvException;
 import org.opencv.core.Mat;
+import java.awt.image.DataBufferByte;
 
-public class LiveTimingGUI extends JPanel {
+public class LiveTimingGUI
+    extends JPanel
+{
 
-    private JButton startB;
-    private JButton stop;
-    private JButton pause;
-    private JButton resume;
-    private JFrame frame;
-    private JLabel label;
+    private JButton              startB;
+    private JButton              stop;
+    private JButton              pause;
+    private JButton              resume;
+    private volatile JFrame      frame;
+    private volatile JLabel      label;
     private ThreadedCameraRunner camera;
-    private Thread runner;
-    private boolean terminated;
+    private Thread               runner;
+    private volatile boolean     terminated;
 
     // start button stop button and run analysis button (create post timing gui
     // and call its run)
-    public LiveTimingGUI() {
+    public LiveTimingGUI()
+    {
         startB = new JButton("Start");
         stop = new JButton("Stop");
         pause = new JButton("Pause");
@@ -36,21 +42,34 @@ public class LiveTimingGUI extends JPanel {
         terminated = false;
     }
 
-    public void refresh(BufferedImage b) {
-        label.setIcon(new ImageIcon(b));
+
+    public void refresh(BufferedImage b)
+    {
+        if (b != null)
+        {
+            label.setIcon(new ImageIcon(b));
+            System.out.println("b is not null");
+        }
+        else
+            System.out.println("b is null");
     }
 
-    public void run() {
+
+    public void run()
+    {
+        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         startB.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent e)
+            {
                 camera.execute();
             }
         });
 
         stop.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent e)
+            {
                 camera.receiveMessage("STOP");
                 terminated = true;
             }
@@ -58,14 +77,16 @@ public class LiveTimingGUI extends JPanel {
 
         pause.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent e)
+            {
                 camera.receiveMessage("PAUSE");
             }
         });
 
         resume.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {
+            public void actionPerformed(ActionEvent e)
+            {
                 camera.receiveMessage("RESUME");
             }
         });
@@ -78,12 +99,62 @@ public class LiveTimingGUI extends JPanel {
         frame.add(this);
         frame.setVisible(true);
 
-        while (!terminated) {
-            refresh(camera.getCompositeFrame().getImage());
+        while (!terminated)
+        {
+            Mat compositeMat = camera.getCompositeFrame().getMat();
+            if (compositeMat == null)
+                System.out.print("\t");
+            else
+            {
+                System.out.println(camera.getCompositeFrame().getMat().size());
+                refresh(matToBufferedImage(compositeMat));
+            }
+
         }
     }
 
-    public static void main(String[] args) {
-        LiveTimingGUI run = new LiveTimingGUI();
+
+    private static BufferedImage matToBufferedImage(Mat m)
+    {
+        System.out.println("in m to b");
+        try
+        {
+            System.out.println("in try");
+            if (m == null)
+                return null;
+            int type = BufferedImage.TYPE_3BYTE_BGR;
+            int bufferSize = m.channels() * m.cols() * m.rows();
+            byte[] b = new byte[bufferSize];
+            try
+            {
+                m.get(0, 0, b); // get all the pixels
+            }
+            catch (java.lang.Exception e)
+            {
+                System.out.println("unknown exception");
+                return null;
+            }
+
+            if (m.cols() == 0 || m.rows() == 0)
+                return null;
+            BufferedImage image = new BufferedImage(m.cols(), m.rows(), type);
+            final byte[] targetPixels =
+                ((DataBufferByte)image.getRaster().getDataBuffer()).getData();
+            System.arraycopy(b, 0, targetPixels, 0, b.length);
+            return image;
+        }
+        catch (CvException cve)
+        {
+            System.out.println("CvException in buffered image conversion");
+            return null;
+        }
+
+    }
+
+
+    public static void main(String[] args)
+    {
+        LiveTimingGUI LTG = new LiveTimingGUI();
+        LTG.run();
     }
 }
