f93a1b52a6ebc9e35b1b3ff3b638de8e0a8f6ace
AdarshRIyer <76015889+adarshramiyer@users.noreply.github.com>
2023-05-22 12:26:23 -0700

    trying to get displaying images to work

--- components/LiveTimingGUI.java
+++ components/LiveTimingGUI.java
@@ -14,23 +14,20 @@ import org.opencv.core.Mat;
 import java.awt.image.DataBufferByte;
 
 public class LiveTimingGUI
-    extends JPanel
-{
+        extends JPanel {
 
-    private JButton              startB;
-    private JButton              stop;
-    private JButton              pause;
-    private JButton              resume;
-    private volatile JFrame      frame;
-    private volatile JLabel      label;
+    private volatile JButton startB;
+    private volatile JButton stop;
+    private volatile JButton pause;
+    private volatile JButton resume;
+    private volatile JFrame frame;
+    private volatile JLabel label;
     private ThreadedCameraRunner camera;
-    private Thread               runner;
-    private volatile boolean     terminated;
+    private volatile boolean terminated;
 
     // start button stop button and run analysis button (create post timing gui
     // and call its run)
-    public LiveTimingGUI()
-    {
+    public LiveTimingGUI() {
         startB = new JButton("Start");
         stop = new JButton("Stop");
         pause = new JButton("Pause");
@@ -42,34 +39,26 @@ public class LiveTimingGUI
         terminated = false;
     }
 
-
-    public void refresh(BufferedImage b)
-    {
-        if (b != null)
-        {
+    public void refresh(BufferedImage b) {
+        if (b != null) {
             label.setIcon(new ImageIcon(b));
             System.out.println("b is not null");
-        }
-        else
+        } else
             System.out.println("b is null");
     }
 
-
-    public void run()
-    {
+    public void run() {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         startB.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e)
-            {
+            public void actionPerformed(ActionEvent e) {
                 camera.execute();
             }
         });
 
         stop.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e)
-            {
+            public void actionPerformed(ActionEvent e) {
                 camera.receiveMessage("STOP");
                 terminated = true;
             }
@@ -77,60 +66,60 @@ public class LiveTimingGUI
 
         pause.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e)
-            {
+            public void actionPerformed(ActionEvent e) {
                 camera.receiveMessage("PAUSE");
             }
         });
 
         resume.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e)
-            {
+            public void actionPerformed(ActionEvent e) {
                 camera.receiveMessage("RESUME");
             }
         });
+
+        Thread refreshThread = new Thread() {
+            @Override
+            public void run() {
+                while (!terminated) {
+                    Mat compositeMat = camera.getCompositeFrame().getMat();
+                    if (compositeMat == null)
+                        System.out.print("\t");
+                    else {
+                        System.out.println(compositeMat.size());
+                        //refresh(matToBufferedImage(compositeMat));
+                    }
+
+                }
+            }
+        };
 
         add(startB);
         add(stop);
         add(pause);
         add(resume);
-        frame.add(label);
         frame.add(this);
         frame.setVisible(true);
-
-        while (!terminated)
-        {
-            Mat compositeMat = camera.getCompositeFrame().getMat();
-            if (compositeMat == null)
-                System.out.print("\t");
-            else
-            {
-                System.out.println(camera.getCompositeFrame().getMat().size());
-                refresh(matToBufferedImage(compositeMat));
-            }
-
-        }
+        refreshThread.start();
     }
 
-
-    private static BufferedImage matToBufferedImage(Mat m)
-    {
+    private static BufferedImage matToBufferedImage(Mat m) {
         System.out.println("in m to b");
-        try
-        {
+        try {
             System.out.println("in try");
             if (m == null)
                 return null;
+            System.out.println("past m null condition");
             int type = BufferedImage.TYPE_3BYTE_BGR;
             int bufferSize = m.channels() * m.cols() * m.rows();
+            System.out.println("buffersize is " + bufferSize);
             byte[] b = new byte[bufferSize];
-            try
-            {
+            System.out.println("made new byte[]");
+            try {
+                System.out.println("trying to get pixels");
                 m.get(0, 0, b); // get all the pixels
-            }
-            catch (java.lang.Exception e)
-            {
+                System.out.println("got pixels");
+            } catch (java.lang.Exception e) {
                 System.out.println("unknown exception");
                 return null;
             }
@@ -138,22 +127,17 @@ public class LiveTimingGUI
             if (m.cols() == 0 || m.rows() == 0)
                 return null;
             BufferedImage image = new BufferedImage(m.cols(), m.rows(), type);
-            final byte[] targetPixels =
-                ((DataBufferByte)image.getRaster().getDataBuffer()).getData();
+            final byte[] targetPixels = ((DataBufferByte) image.getRaster().getDataBuffer()).getData();
             System.arraycopy(b, 0, targetPixels, 0, b.length);
             return image;
-        }
-        catch (CvException cve)
-        {
+        } catch (CvException cve) {
             System.out.println("CvException in buffered image conversion");
             return null;
         }
 
     }
 
-
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         LiveTimingGUI LTG = new LiveTimingGUI();
         LTG.run();
     }
---
+++ tests/LiveDisplayTest.java
@@ -0,0 +1,89 @@
+package tests;
+
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JFrame;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.imgcodecs.Imgcodecs;
+import java.io.InputStream;
+import java.io.ByteArrayInputStream;
+
+import components.*;
+
+public class LiveDisplayTest extends JPanel {
+
+    private ThreadedCameraRunner tcr;
+    private JFrame frame;
+    private JLabel picLabel;
+
+    private BufferedImage b;
+
+    public LiveDisplayTest() {
+        frame = new JFrame();
+        frame.setSize(500, 500);
+    }
+
+    public void print(String s) {
+        System.out.println(s);
+    }
+
+    public void singleImageTest() {
+        try {
+            b = ImageIO.read(new File("C:\\Users\\adars\\Downloads\\download.png"));
+        }
+        catch(IOException ioex) {
+            System.out.println("IO Exception occurred");
+            return;
+        }
+
+        picLabel = new JLabel(new ImageIcon(b));
+        picLabel.setIcon(new ImageIcon(b));
+        add(picLabel);
+        frame.add(this);
+        frame.setVisible(true);
+        System.out.println("complete");
+    }
+
+    public void setUpTCR() {
+        tcr = new ThreadedCameraRunner();
+        print("tcr set up");
+    }
+
+    public void runLive() throws IOException{
+        tcr.execute();
+        print("executing");
+        while (true) {
+            Mat compositeMat = tcr.getCompositeFrame().getMat();
+            BufferedImage liveImage = Mat2BufferedImage(compositeMat);
+            picLabel.setIcon(new ImageIcon(b));
+        }
+    }
+
+    public BufferedImage Mat2BufferedImage(Mat mat) throws IOException{
+        //Encoding the image
+        MatOfByte matOfByte = new MatOfByte();
+        Imgcodecs.imencode(".jpg", mat, matOfByte);
+        //Storing the encoded Mat in a byte array
+        byte[] byteArray = matOfByte.toArray();
+        //Preparing the Buffered Image
+        InputStream in = new ByteArrayInputStream(byteArray);
+        BufferedImage bufImage = ImageIO.read(in);
+        return bufImage;
+     }
+
+    
+}
+
+class Runner {
+    public static void main(String[] args) {
+        LiveDisplayTest ldt = new LiveDisplayTest();
+        ldt.setUpTCR();
+        ldt.runLive();
+    }
+}
