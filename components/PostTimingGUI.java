74f48c4b2d4690636697bfae659ee3936b8e6f1d
adarshramiyer <adarshramiyer@users.noreply.github.com>
2023-05-24 17:00:54 +0000

    Apply formatting changes

--- components/PostTimingGUI.java
+++ components/PostTimingGUI.java
@@ -7,26 +7,23 @@ import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.image.BufferedImage;
 import java.util.*;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 import org.opencv.core.*;
-import javax.swing.ImageIcon;
 
-public class PostTimingGUI
-    extends JPanel
-{
-    private ArrayList<DraggableLine>      finishes;
-    private static CompositeFrame         finishImage;
+public class PostTimingGUI extends JPanel {
+    private ArrayList<DraggableLine> finishes;
+    private static CompositeFrame finishImage;
     private static ArrayList<SingleFrame> OCRstream;
-    private OutputProcessor               processor;
-    private int                           check = 0;
-    private JFrame                        frame;
+    private OutputProcessor processor;
+    private int check = 0;
+    private JFrame frame;
 
-    public PostTimingGUI(CompositeFrame image, ArrayList<SingleFrame> ocr)
-    {
+    public PostTimingGUI(CompositeFrame image, ArrayList<SingleFrame> ocr) {
         // TODO complete constructor
 
         OCRstream = ocr;
@@ -36,25 +33,17 @@ public class PostTimingGUI
         processor = new OutputProcessor(finishes);
     }
 
-
-    public void addListener()
-    {
+    public void addListener() {
         addMouseListener(new MouseListener() {
             @Override
-            public void mouseClicked(java.awt.event.MouseEvent e)
-            {
-                if (SwingUtilities.isLeftMouseButton(e))
-                {
+            public void mouseClicked(java.awt.event.MouseEvent e) {
+                if (SwingUtilities.isLeftMouseButton(e)) {
                     finishes.add(new DraggableLine(new TimeFormat(), 5, e.getX()));
                     // PostTimingGUI.this.removeAll();
                     repaint();
-                }
-                else if (SwingUtilities.isRightMouseButton(e))
-                {
-                    for (int i = 0; i < finishes.size(); i++)
-                    {
-                        if (finishes.get(i).getXPos() == e.getX())
-                        {
+                } else if (SwingUtilities.isRightMouseButton(e)) {
+                    for (int i = 0; i < finishes.size(); i++) {
+                        if (finishes.get(i).getXPos() == e.getX()) {
                             finishes.remove(finishes.get(i));
                             // PostTimingGUI.this.removeAll();
                             repaint();
@@ -63,23 +52,15 @@ public class PostTimingGUI
                 }
             }
 
-
             @Override
-            public void mousePressed(java.awt.event.MouseEvent e)
-            {
+            public void mousePressed(java.awt.event.MouseEvent e) {
                 check = e.getX();
             }
 
-
             @Override
-            public void mouseReleased(java.awt.event.MouseEvent e)
-            {
-                for (int i = 0; i < finishes.size(); i++)
-                {
-                    if (Math.abs(
-                        finishes.get(i).getXPos()
-                            - check) <= 5 /* Threshold for click error */)
-                    {
+            public void mouseReleased(java.awt.event.MouseEvent e) {
+                for (int i = 0; i < finishes.size(); i++) {
+                    if (Math.abs(finishes.get(i).getXPos() - check) <= 5 /* Threshold for click error */) {
                         finishes.get(i).changeXPos(e.getX());
                         // getOCR(finishes.get(i).getXPos()); This line is OK,
                         // just need to disable while testing
@@ -90,23 +71,17 @@ public class PostTimingGUI
                 }
             }
 
-
             @Override
-            public void mouseEntered(java.awt.event.MouseEvent e)
-            {
+            public void mouseEntered(java.awt.event.MouseEvent e) {
             }
 
-
             @Override
-            public void mouseExited(java.awt.event.MouseEvent e)
-            {
+            public void mouseExited(java.awt.event.MouseEvent e) {
             }
         });
     }
 
-
-    public void paint(Graphics g)
-    {
+    public void paint(Graphics g) {
         super.paint(g);
         /*if (finishImage != null)
         {
@@ -116,15 +91,12 @@ public class PostTimingGUI
         frame.pack();
 
         g.setColor(Color.RED);
-        for (int i = 0; i < finishes.size(); i++)
-        {
+        for (int i = 0; i < finishes.size(); i++) {
             g.drawLine(finishes.get(i).getXPos(), 0, finishes.get(i).getXPos(), this.getHeight());
         }
     }
 
-
-    private int getOCR(int xPos)
-    {
+    private int getOCR(int xPos) {
         // TODO don't worry about this one right now
         // each draggable line gets the ocr, every time the draggable line is
         // moved
@@ -132,20 +104,16 @@ public class PostTimingGUI
         // image
         // call the athlete ocr on it
         int i = 0;
-        for (DraggableLine d : finishes)
-        {
-            if (d.getXPos() == xPos)
-            {
+        for (DraggableLine d : finishes) {
+            if (d.getXPos() == xPos) {
                 d.updateTimestamp();
                 break;
             }
             i++;
         }
         Mat ret = null;
-        for (SingleFrame f : OCRstream)
-        {
-            if (f.getTime() == finishes.get(i).getTimestamp())
-            {
+        for (SingleFrame f : OCRstream) {
+            if (f.getTime() == finishes.get(i).getTimestamp()) {
                 ret = f.getMat();
             }
         }
@@ -153,16 +121,13 @@ public class PostTimingGUI
         return getAthleteNumber(ret);
     }
 
-
-    public void run()
-    {
+    public void run() {
         // TODO GUI code, treat like a main method
 
         addListener();
         frame = new JFrame();
         frame.setSize(1000, 500);
-        if (finishImage != null)
-        {
+        if (finishImage != null) {
             add(new JLabel(new ImageIcon(finishImage.getImage())));
         }
         frame.add(this);
@@ -171,13 +136,8 @@ public class PostTimingGUI
     }
 }
 
-
-
-
-class PTGTester
-{
-    public static void main(String[] args)
-    {
+class PTGTester {
+    public static void main(String[] args) {
         PostTimingGUI PTG = new PostTimingGUI(null, null);
         PTG.run();
     }
